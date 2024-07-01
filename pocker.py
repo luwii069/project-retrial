02093931a539f21af02a3f295b83f44b79cd4998
louis mulaa <louismulaa06@gmail.com>
2024-07-01 11:38:00 +0300

    trying out quiting command

--- pocker.py
+++ pocker.py
@@ -1,12 +1,14 @@
 import random
 
 # Define ranks and suits for the deck
-ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A',]
 suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
 
 #new deck to ensure the table card does is'nt a special card
 new_ranks= ['4', '5', '6', '7', '9', '10']
 new_suits= ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+
+joker = [('joker', 'joker')]
 
 # Function to create and shuffle a deck of cards
 def create_deck():
@@ -15,6 +17,7 @@ def create_deck():
 #function that treates a new deck without the special cards
 def new_deck():
     return [(new_rank, new_suit) for new_suit in new_suits for new_rank in new_ranks]
+        
 
 # Function to deal cards to players
 def deal_cards(deck, num_cards):
@@ -26,15 +29,11 @@ def display_cards(cards, player_name):
     for i, card in enumerate(cards):
         print(f"{i + 1}: {card[0]} of {card[1]}")
 
-# Function to check if a card matches the table card
-def is_match(card, table_card):
-    return card[0] == table_card[0] or card[1] == table_card[1]
-
-# Main function to run the game
 def main():
     # Create and shuffle the deck
     deck = create_deck()
     deck2 = new_deck()
+    deck.append(joker)
     random.shuffle(deck)
     random.shuffle(deck2)
 
@@ -44,34 +43,69 @@ def main():
 
     # Place one card from the deck face up on the table
     tablecard = [deck2.pop()]
+    #print(deck)
+    
+
 
     while True:
-        playermoves(player_hand, computer_hand, tablecard, deck,deck2)
-        computer_turn(player_hand, computer_hand, tablecard, deck,deck2)
+        try:
 
-def playermoves(player_hand, computer_hand, tablecard, deck,deck2):
+            playermoves(player_hand, computer_hand, tablecard, deck)
+            computer_turn(player_hand, computer_hand, tablecard, deck)
+        except Exception as e:
+            print("error occured: ", e)
+
+def playermoves(player_hand, computer_hand, tablecard, deck):
     print(f"Your hand: {player_hand}")
     print(f"Computer's hand: {computer_hand}")
-    print(f"table Card: {tablecard}")
+    print(f"table Cards: {tablecard}")
 
     rank = input("Enter the card rank: ").capitalize().strip()
     suit = input("Enter the card suit: ").capitalize().strip()
+   
+
 
     play = (rank, suit)
 
-    if play in player_hand and (play[0] == tablecard[-1][0] or play[1] == tablecard[-1][1]):
-        if (tablecard[-1][0] in ['3', '2'] and play[0]!= 'Ace'): 
-            for _ in range(int(tablecard[-1][0])): 
-                player_hand.append(deck.pop())
-            return
+    if play in player_hand and (play[0] == tablecard[-1][0] or play[1] == tablecard[-1][1]): 
+     
         tablecard.append(play)
         player_hand.remove(play)
-        if play[0] == "Ace":
+        if play[0] == "A":
             newsuit = input("Enter the new card suit: ").capitalize().strip()
             tablecard[-1] = (tablecard[-1][0], newsuit)
             print(f"The game was changed to {newsuit}")
-        if play[0] in ['8', 'King', 'Jack', 'Queen']:
-            return playermoves(player_hand, computer_hand, tablecard, deck,deck2)
+
+        if play[0] in ["2"]:
+                for _ in range(2):
+                    if deck:
+                        computer_hand.append(deck.pop())
+                    print("Penalty!")
+                    print("Cards added")
+                        
+                
+        if play[0] in ["3"]:
+                for _ in range(3):
+                    if deck:
+                        computer_hand.append(deck.pop())
+                    print("Penalty!") 
+                    print("Cards added")
+                       
+        
+        if play[0] =="k" or play[0] == 'J':
+            playermoves(player_hand, computer_hand, tablecard, deck)
+            computer_turn(player_hand, computer_hand, tablecard, deck)
+
+        if play[0] == "8" or play[0] == "Q":
+            playermoves(player_hand, computer_hand, tablecard, deck)
+
+        
+        if play[0] == "Joker" or play[1] == "Joker":
+            for _ in range(5):
+                computer_hand.append(deck.pop())
+
+
+
             
     elif rank == "Pick": 
         player_hand.append(deck.pop())
@@ -79,43 +113,55 @@ def playermoves(player_hand, computer_hand, tablecard, deck,deck2):
         
     else:
         print(f"Card {play} is not playable. Please try again.")
-        playermoves(player_hand, computer_hand, tablecard, deck,deck2)
+        playermoves(player_hand, computer_hand, tablecard, deck)
 
-def computer_turn(player_hand, computer_hand, tablecard, deck, deck2):
-    while True:
-        # Choose a random card from the computer's hand
-        play = random.choice(computer_hand)
+def computer_turn(player_hand, computer_hand, tablecard, deck):
+        
+    playable_cards = [card for card in computer_hand if card[0] == tablecard[-1][0] or card[1] == tablecard[-1][1]]
+    if playable_cards:
+        play = random.choice(playable_cards)
 
-        # Check if the card matches the top card on the table
-        if is_match(play, tablecard[-1]):
-            # If the card has a special effect, apply it
-            if play[0] in ['3', '2']:
-                for _ in range(int(play[0])):
-                    computer_hand.append(deck.pop())
-            tablecard.append(play)
-            computer_hand.remove(play)
-            if play[0] == "Ace":
-                newsuit = random.choice(suits)
-                tablecard[-1] = (tablecard[-1][0], newsuit)
-                print(f"The game was changed to {newsuit}")
-            if play[0] in ['8', 'King', 'Jack', 'Queen']:
-                return computer_turn(player_hand, computer_hand, tablecard, deck, deck2)
-            break
-        else:
-            # If the card doesn't match, try another card
-            continue
-
-    # If the computer has no cards left, it loses the game
-    if not computer_hand:
-        print("The computer has no cards left. You win!")
+    else:
+        if deck:
+            computer_hand.append(deck.pop())
+            print("Computer picked a card!")
         return
 
-    print(f"Computer played: {play}")
+    tablecard.append(play)
+    computer_hand.remove(play)
+
+    if play[0] in ["2"]:
+        for _ in range(2):
+            if deck:
+                player_hand.append(deck.pop())
+            print("Penalty!")
+            print("Cards added")
+           
+                
+    if play[0] in ["3"]:
+        for _ in range(3):
+            if deck:
+                player_hand.append(deck.pop())
+            print("Penalty!")
+            print("Cards added")
+                
+    
+    if play[0] =="k" or play[0] == 'J':
+            computer_turn(player_hand, computer_hand, tablecard, deck)
+            playermoves(player_hand, computer_hand, tablecard, deck)
+
+    if play[0] == "8" or play[0] == "Q":
+            computer_turn(player_hand, computer_hand, tablecard, deck)
+
+    if play[0] == "Joker" or play[1] == "Joker":
+            for _ in range(5):
+                computer_hand.append(deck.pop())
+
+
+ 
     print(f"Computer's hand: {computer_hand}")
-    print(f"Table cards: {tablecard}")
+    print(f"table Cards: {tablecard}")
 
-
-
-
+   
 if __name__ == "__main__":
     main()
